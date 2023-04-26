a53fdc376bc11e06366acf79a9ee88683289f2d7
LuisFloresA <Luisp.floresa@gmail.com>
2023-04-25 20:56:25 -0400

    agregado sopaipas

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,6 @@ miTitulo.innerHTML = "Hola Comidas";
 
 var subtitle = document.getElementById("subtitle");
 subtitle.innerText = "Todos tus platos favoritos";
+
+var com7 = document.getElementById("com7");
+com7.innerText = "Sopaipas";
