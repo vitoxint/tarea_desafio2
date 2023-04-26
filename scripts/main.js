bd82ca5c569f654a8b0ed1e0e410e63c226e3432
vitoxint <vitoxint@gmail.com>
2023-04-25 20:42:45 -0400

    modifica id h5

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+var miTitulo = document.querySelector("h1");
+miTitulo.innerHTML = "Hola Comidas";
+
+var subtitle = document.getElementById("subtitle");
+subtitle.innerText = "Todos tus platos favoritos";
